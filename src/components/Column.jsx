7dc141df4e11e2a729deaa17aa4c623888cf7d2a
Drakkarrr <drakkar.alpha00@gmail.com>
2024-02-02 10:04:50 -0800

    task css and component ADDED

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,7 +1,13 @@
+import Task from './Task';
 import './column.css';
 
 const Column = ({ state }) => {
-  return <div className='column'>{state}</div>;
+  return (
+    <div className='column'>
+      <p>{state}</p>
+      <Task title='Task 1' />
+    </div>
+  );
 };
 
 export default Column;
