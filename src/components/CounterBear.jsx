34468859c69bc0ce7eccefbf335bea520c29cd47
Drakkarrr <drakkar.alpha00@gmail.com>
2024-02-03 08:10:16 -0800

    complete success bear state

---
+++ src/components/CounterBear.jsx
@@ -0,0 +1,13 @@
+import BearCounter from './BearCounter';
+import Controls from './Control';
+
+const CounterBear = () => {
+  return (
+    <>
+      <BearCounter />
+      <Controls />
+    </>
+  );
+};
+
+export default CounterBear;
