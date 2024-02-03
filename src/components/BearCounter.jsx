dfbc63e2d9ee9ab6bce41c75ca585f6250fcaf3f
Drakkarrr <drakkar.alpha00@gmail.com>
2024-02-03 08:02:35 -0800

    counter useStore zustand

---
+++ src/components/BearCounter.jsx
@@ -0,0 +1,8 @@
+import { useStore } from 'zustand';
+
+function BearCounter() {
+  const bears = useStore((state) => state.bears);
+  return <h1>{bears} around here...</h1>;
+}
+
+export default BearCounter;
